ad83ac800deb0aeed5af59664f68f30cab291b00
AlexK <kuzielalex@gmail.com>
2024-03-26 02:56:40 +0300

    added get request and port listen

--- budgetprogram.js
+++ budgetprogram.js
@@ -2,3 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.get('/', (req, res,next) => {
+    console.log('Hello world!')
+})
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+})
