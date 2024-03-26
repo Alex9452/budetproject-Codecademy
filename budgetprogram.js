c34c1f88f4fdf003f72d7de32543b5c7ab6cdf5b
AlexK <kuzielalex@gmail.com>
2024-03-27 02:05:27 +0300

    added app.use and get envolope by name

--- budgetprogram.js
+++ budgetprogram.js
@@ -2,14 +2,19 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const envolopes = []
+
 app.get('/', (req, res, next) => {
     res.send('Hello world!')
+})
+
+app.get('/envelopes', (req, res, next) => {
+    res.send(evolopes)
 })
 
 app.post('/envelopes', (req,res, next) => {
     res.send('Your information has been sent succesfully')
     res.status(200)
-
 })
 
 app.listen(port, () => {
