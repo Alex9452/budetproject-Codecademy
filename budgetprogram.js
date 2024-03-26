5ebb9db65ab6f609bbc1b4d6daddbe3289913081
AlexK <kuzielalex@gmail.com>
2024-03-26 03:31:08 +0300

    added app.post

--- budgetprogram.js
+++ budgetprogram.js
@@ -2,8 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res,next) => {
+app.get('/', (req, res, next) => {
     res.send('Hello world!')
+})
+
+app.post('/envelopes', (req,res, next) => {
+    res.send('Your information has been sent succesfully')
+    res.status(200)
+
 })
 
 app.listen(port, () => {
