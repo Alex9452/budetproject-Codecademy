c13d3076536c30bb08fda98029d6139e122576a4
AlexK <kuzielalex@gmail.com>
2024-03-27 02:12:23 +0300

    added app.post

--- budgetprogram.js
+++ budgetprogram.js
@@ -2,19 +2,38 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-const envolopes = []
+const envolopes = ['']
+
+app.use(express.static('public'))
+
+
 
 app.get('/', (req, res, next) => {
     res.send('Hello world!')
 })
 
-app.get('/envelopes', (req, res, next) => {
-    res.send(evolopes)
+app.get('/envolopes', (req, res, next) => {
+    res.send(envolopes)
+    
 })
 
-app.post('/envelopes', (req,res, next) => {
-    res.send('Your information has been sent succesfully')
-    res.status(200)
+app.get('/envolopes/:name', (req, res, next) => {
+    const person = getElementById(req.param.name,envolopes)
+    res.send(person)
+})
+
+app.post('/envolopes', (req,res, next) => {
+    const newPerson = createElement('envolopes', req.query)
+    if (newPerson) {
+        envolopes.push(newPerson)
+        res.status(201).send(newPerson)
+        res.send('Your information has been sent succesfully!')
+    } else {
+        res.status(400).send()
+        res.send('There has been an error! Please try again.')
+    }
+    
+    
 })
 
 app.listen(port, () => {
