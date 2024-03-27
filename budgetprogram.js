b67f9dcde1378665226dd2abef5598c95c5aa6db
AlexK <kuzielalex@gmail.com>
2024-03-28 02:20:45 +0300

    added put request

--- budgetprogram.js
+++ budgetprogram.js
@@ -1,15 +1,13 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const {
-    getElementById,
-    getIndexById,
-    updateElement,
-    seedElements,
-    createElement
-} = require("./utils")
-
-const envolopes = ['']
+// const {getElementById, getIndexById, createElement} = require('/utils')
+const envolopes = {
+    'dining out':
+    300,
+    groceries:
+    400
+}
 
 app.use(express.static('public'))
 
@@ -19,17 +17,17 @@ app.get('/', (req, res, next) => {
     res.send('Hello world!')
 })
 
-app.get('/envolopes', (req, res, next) => {
+app.get('http://localhost:3000/envolopes', (req, res, next) => {
     res.send(envolopes)
     
 })
 
-app.get('/envolopes/:name', (req, res, next) => {
+app.get('http://localhost:3000/envolopes/:name', (req, res, next) => {
     const person = getElementById(req.param.name,envolopes)
     res.send(person)
 })
 
-app.post('/envolopes', (req,res, next) => {
+app.post('http://localhost:3000/envolopes', (req,res, next) => {
     const newPerson = createElement('envolopes', req.query)
     if (newPerson) {
         envolopes.push(newPerson)
@@ -40,8 +38,22 @@ app.post('/envolopes', (req,res, next) => {
         res.send('There has been an error! Please try again.')
     }
 })
+//transfering between envolopes
+app.post('http://localhost:3000/envolopes/transfer/:from/:to', (req ,res, next) => {
 
-app.delete("/envolopes/:name", (req, res, next) => {
+})
+
+app.put('http://localhost:3000/envolopes',(req, res, next) => {
+    const envolopeUpdates = getIndexById(req.params.id,envolopes)
+    if(envolopeUpdates !== -1){
+        updateElement(req.params.id, req.query, envolopes)
+        res.send(envolopes(envolopeUpdates))    
+    } else {
+        res.status('404').send()
+    }
+})
+
+app.delete("http://localhost:3000/envolopes/:name", (req, res, next) => {
     const nameIndex = getIndexById(req.params.name, envolopes)
     if(nameIndex !== -1) {
         envolopes.splice(nameIndex,1)
