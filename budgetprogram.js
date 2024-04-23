346de50b53af31bf14b36d47bfacf4044cc4b4eb
Alex K <kuzielalex@gmail.com>
2024-04-24 02:38:24 +0300

    update

--- budgetprogram.js
+++ budgetprogram.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 5432
- {getElementById, getIndexById, createElement} = require('/utils')
+ //{getElementById, getIndexById, createElement} = require('/utils')
 app.use(express.static('public'))
 
 
