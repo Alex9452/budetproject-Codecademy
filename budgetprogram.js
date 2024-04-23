022f6fa7681036d1f06353fd1c611d088754d39f
Alex K <kuzielalex@gmail.com>
2024-04-23 19:38:38 +0300

    fixed spelling errors

--- budgetprogram.js
+++ budgetprogram.js
@@ -1,71 +1,63 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 5432
 // const {getElementById, getIndexById, createElement} = require('/utils')
 app.use(express.static('public'))
-const envolopes = {
-    diningOut: 300,
-    groceries: 400,
-    rent: 1600,
-    automotive: 375,
-    savings: 600,
-    projects: 475
 
-}
 
 
 app.get('/', (req, res, next) => {
     res.send('Hello world!')
 })
 
-app.get('/envolopes', (req, res, next) => {
-    res.send(envolopes)
+app.get('/envelopes', (req, res, next) => {
+    res.send(envelopes)
     
 })
 
-app.get('/envolopes/:id', (req, res, next) => {
-    const name = getElementById(req.param.id,envolopes)
-    res.send(person)
+app.get('/envelopes/:id', (req, res, next) => {
+    const name = getElementById(req.param.id,envelopes)
+    res.send(name)
 })
 
-app.post('/envolopes', (req,res, next) => {
-    const newPerson = push('envolopes', req.query)
-    if (newPerson) {
-        envolopes.push(newPerson)
-        res.status(201).send(newPerson)
+app.post('/envelopes', (req,res, next) => {
+    const newEnvelope = push('envelopes', req.query)
+    if (newEnvelope) {
+        envelopes.push(newEnvelope)
+        res.status(201).send(newEnvelope)
         res.send('Your information has been sent succesfully!')
     } else {
         res.status(400).send()
         res.send('There has been an error! Please try again.')
     }
 })
-//transfering between envolopes
-app.post('/envolopes/transfer/:from/:to', (req ,res, next) => {
-    const fromEnvolope = req.params.name
-    const toEnvolope = req.params.name
+//transfering between envelopes
+app.post('/envelopes/transfer/:from/:to', (req ,res, next) => {
+    const fromEnvelope = req.params.name
+    const toEnvelope = req.params.name
     const moneyEx = ''
-    if (fromEnvolope == toEnvolope){
+    if (fromEnvelope == toEnvelope){
         res.send('Can not be same Envolope')
     } else {
-        fromEnvolope - moneyEx
-        toEnvolope + moneyEx
+        fromEnvelope - moneyEx
+        toEnvelope + moneyEx
     }
 })
 
-app.put('/envolopes',(req, res, next) => {
-    const envolopeUpdates = getIndexById(req.params.id,envolopes)
-    if(envolopeUpdates !== -1){
-        updateElement(req.params.name, req.query, envolopes)
-        res.send(envolopes(envolopeUpdates))    
+app.put('/envelopes',(req, res, next) => {
+    const envelopeUpdates = getIndexById(req.params.id,envelopes)
+    if(envelopeUpdates !== -1){
+        updateElement(req.params.name, req.query, envelopes)
+        res.send(envelopes(envelopeUpdates))    
     } else {
         res.status('404').send()
     }
 })
 
-app.delete("/envolopes/:id", (req, res, next) => {
+app.delete("/envelopes/:id", (req, res, next) => {
     const nameIndex = getIndexById(req.params.name, envolopes)
     if(nameIndex !== -1) {
-        envolopes.splice(nameIndex,1)
+        envelopes.splice(nameIndex,1)
         res.status(204).send(nameIndex)
     } else {
         res.status(404).send()
